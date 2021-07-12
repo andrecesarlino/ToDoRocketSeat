834270d8dfbd0cfc7858f7ae05e4641241146d60
Andre Lino <andreclino@gmail.com>
2021-07-11 22:05:44 -0300

    primeiro

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -7,10 +7,10 @@ interface TodoInputProps {
 }
 
 export function TodoInput({ addTask }: TodoInputProps) {
-  // const [task, setTask] = useState('');
+  const [task, setTask] = useState('');
 
-  function handleAddNewTask() {
-    //TODO - Call addTask if task not empty and clean input value 
+  function handleAddNewTask() {  
+    //setTask([...task, addTask]);
   }
 
   return (
@@ -21,6 +21,8 @@ export function TodoInput({ addTask }: TodoInputProps) {
         placeholderTextColor="#B2B2B2"
         returnKeyType="send"
         selectionColor="#666666"
+        onChangeText={setTask}
+        //onSubmitEditing={handleAddNewTask()}
         //TODO - use value, onChangeText and onSubmitEditing props
       />
       <TouchableOpacity
